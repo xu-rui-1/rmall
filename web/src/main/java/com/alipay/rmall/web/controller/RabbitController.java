5bd19f7ae5cb23e38d00a7b199db410ce25a5f39
锐途 <ruitu.xr@antgroup.com>
2023-05-25 21:09:52 +0800

    fix issue: 添加消息发送服务以及消息回调及回退消息配置

---
+++ web/src/main/java/com/alipay/rmall/web/controller/RabbitController.java
@@ -0,0 +1,37 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2023 All Rights Reserved.
+ */
+package com.alipay.rmall.web.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alipay.rmall.base.config.RabbitMqConfig;
+import com.alipay.rmall.web.vo.ProductBaseInfoVO;
+import io.swagger.annotations.Api;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.UUID;
+
+/**
+ * @author ruitu.xr
+ * @version RabbitController.java, v 0.1 2023年05月25日 21:03 ruitu.xr Exp $
+ */
+@RestController
+@Api(tags = "Rabbit消息发送测试接口")
+public class RabbitController {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    @PostMapping("/sendMessage")
+    public void sendMessage(@RequestBody ProductBaseInfoVO productBaseInfoVO) {
+        String msgId = UUID.randomUUID().toString().replaceAll("-", "");
+        rabbitTemplate.convertAndSend(
+                RabbitMqConfig.DIRECT_EXCHANGE, RabbitMqConfig.DIRECT_ROUTING_KEY, JSON.toJSONString(productBaseInfoVO), new CorrelationData(msgId));
+    }
+}

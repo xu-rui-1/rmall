7f828053854ef71893ee2718cfc08d38c4799e86
锐途 <ruitu.xr@antgroup.com>
2023-05-25 21:01:45 +0800

    fix issue: 添加消息发送服务以及消息回调及回退消息配置

---
+++ service/src/main/java/com/alipay/rmall/service/RabbitService.java
@@ -0,0 +1,10 @@
+package com.alipay.rmall.service;
+
+/**
+ * @author ruitu.xr
+ * @version RabbitService.java, v 0.1 2023年05月25日 20:53 ruitu.xr Exp $
+ */
+public interface RabbitService {
+
+    public void sendMessage2Email(String message);
+}
